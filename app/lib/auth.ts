de4e4591648db144bf073940f5b27dd75e73eb41
“ppedroandrade” <pedrosalvaro09@gmail.comgit config --global user.email pedrosalvaro09@gmail.com>
2025-04-17 17:37:44 -0300

    FEAT: DB conection

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,8 +1,13 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 import NextAuth from "next-auth";
 import  Google from "next-auth/providers/google";
+import { firebaseCert } from "./firebase";
 
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
   providers: [ Google],
+  adapter: FirestoreAdapter({
+    credential: firebaseCert
+  }),
   
 });
